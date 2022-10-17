af060dd769ee05d036b6e3d9613d32db484fd1f7
ZSW <2966211270@qq.com>
2022-10-17 16:20:47 +0800

    vue 自定义指令图片懒加载

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <imgLazy msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import imgLazy from './components/imgLazy.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    imgLazy
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  display: flex;
+  justify-content: center;
 }
 </style>
